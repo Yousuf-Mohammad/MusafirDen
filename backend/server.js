3c79504d37dbd1e7f216f27c3a1347f98e396e05
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2023-09-14 14:11:50 +0600

    mongoose installed , schemas are created

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
 import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
+import connetDB from './config/db.js';
+
 dotenv.config();
-
+connetDB();
 const port = process.env.PORT;
-
 const app = express();
 
 app.get('/',(req, res)=>{
     res.send("API is Running ")
 
 });
+
 app.get('/api/products',(req, res)=>{
     res.json(products);
 
 });
+
 app.get('/api/products/:id',(req, res)=>{
     const product = products.find((p)=>p._id==req.params.id);
     res.json(product);
