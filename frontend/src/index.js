77422910ec7e1ab8a22d14de3b56ace438022432
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2023-09-16 23:26:37 +0600

    redux included and contants and redux store created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './assets/CSS/bootstrap.custom.css';
 import './assets/CSS/index.css';
 import './index.css';
@@ -13,6 +14,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import store from './store';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,7 +29,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router= {router} />
+    <Provider store={store}>
+      <RouterProvider router= {router} />
+    </Provider>
   </React.StrictMode>
 );
 
