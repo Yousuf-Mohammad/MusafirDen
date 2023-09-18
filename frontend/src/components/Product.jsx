692d40250530575d360b19892e35d59875b300fc
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2023-09-19 00:11:19 +0600

    redux slice created

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -5,26 +5,23 @@ import Rating from './Rating'
 const Product = ({ product }) => {
   return (
     <>
-    <Card className='my-3 p-3 rounded '>
+    <Card className='my-3 p-3 rounded productCard '>
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} height={350} variant='top'/>
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
-            <strong> {product.name}</strong>
+          <Card.Title as="h6" className='product-title'>
+            {product.name}
           </Card.Title>
-
           <Card.Text as='div'>
             <Rating value={product.rating} text={product.numReviews}/>
           </Card.Text>
-
-          <Card.Text as='h3'>
-              ${product.price}
+          <Card.Text as='h5'>
+          {product.price}BDT
           </Card.Text>
         </Link>
       </Card.Body>
-
     </Card>
     </>
   )
