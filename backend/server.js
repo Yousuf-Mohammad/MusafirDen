eee6e8fe88bca242543396c12b366d5e8f40ab3b
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2023-10-13 17:08:22 +0600

    build script and other fixes

--- backend/server.js
+++ backend/server.js
@@ -29,11 +29,25 @@ app.use('/api/users', userRoutes)
 app.use('/api/orders',orderRoutes)
 app.use('/api/upload',uploadRoutes)
 
-const __dirname = path.resolve(); //Set __dirname to current directory
-app.use('/uploads',express.static(path.join(__dirname,'/uploads')));
-app.use(notFound);
-app.use(errorHandler);
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+    } else {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    });
+    }
 
+    app.use(notFound);
+    app.use(errorHandler);
 
-
-app.listen(port, ()=>{console.log(`Server is running on port ${port}`)})
+    app.listen(port, () =>
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+    );
