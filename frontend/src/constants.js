ebd27f8e0aa23fd8c59fba4b898830a8531f2960
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2023-10-21 21:21:06 +0600

    cors is still blocking

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV==='development'? 'http://localhost:3000':'http://localhost:8000'||'https://musafir.onrender.com';
+export const BASE_URL = process.env.NODE_ENV==='development'? 'http://localhost:3000':'https://musafir.onrender.com';
 
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
