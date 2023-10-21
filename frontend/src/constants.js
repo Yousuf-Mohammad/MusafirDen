f3fd14867e0bf0169e68278be699be40f5f44bb0
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2023-10-21 21:43:20 +0600

    cors added successfully

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://musafir.onrender.com';
+export const BASE_URL = process.env.NODE_ENV==='development'? 'http://localhost:3000':'*';
 
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
