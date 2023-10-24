d36004093df7c70a4baa50abd2e9032252aa5ef0
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2023-10-24 13:32:15 +0600

    netlify & render deploy

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors())
+app.use(cors());
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
+app.use('/api/orders', orderRoutes); 
 app.use('/api/upload', uploadRoutes);
 
 
